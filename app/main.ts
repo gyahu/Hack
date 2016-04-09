9497dcc45ad52f68d1c28ee86fa186d7b1864dc6
Stefano Gioia <stefanogch@gmail.com>
2016-04-09 14:26:12 -0400

    init commit

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu.component';
+
+bootstrap(AppComponent);
+bootstrap(MenuComponent);
---
+++ app/menu.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app-menu',
+    template:`
+    `
+})
+
+export class MenuComponent { 
+	app_name = 'Name';
+  	username_label = 'Windstorm';
+  	password_label = "Password";
+}
