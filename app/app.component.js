7fc90ff35a8559240d971fd0c49b05e8adafa446
Stefano Gioia <stefanogch@gmail.com>
2016-04-10 02:34:14 -0300

    can do log in request

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './home.component', './profile.component', './register.component', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core', './home.component', './profile.component', './register.component', 'angular2/http', 'angular2/common', 'angular2/router', 'rxjs/Rx'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './home.component', './profile.component', './
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, home_component_1, profile_component_1, register_component_1, router_1;
+    var core_1, home_component_1, profile_component_1, register_component_1, http_1, common_1, router_1;
     var AppComponent;
     return {
         setters:[
@@ -26,22 +26,44 @@ System.register(['angular2/core', './home.component', './profile.component', './
             function (register_component_1_1) {
                 register_component_1 = register_component_1_1;
             },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
+            },
             function (router_1_1) {
                 router_1 = router_1_1;
-            }],
+            },
+            function (_1) {}],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(__router, http) {
+                    this.__router = __router;
+                    this.http = http;
                     this.app_name = 'THE NAME';
                     this.username_label = 'Username';
                     this.password_label = "Password";
                     this.log_in_label = "Log In";
+                    this.logged_in = false;
                 }
+                AppComponent.prototype.onSubmit = function (data) {
+                    var _this = this;
+                    var headers = new http_1.Headers();
+                    headers.append('Contencdt-Type', 'application/json');
+                    this.data = JSON.stringify(data);
+                    this.http.post("http://192.168.0.58:8000/users/login/", this.data, headers)
+                        .map(function (res) { return JSON.stringify(res.json()); })
+                        .subscribe(function (data) { return _this.resp = data; }, function (err) { return _this.logError(err); });
+                };
+                AppComponent.prototype.logError = function (err) {
+                    console.error('There was an error: ' + err);
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n  <div id=\"menu\">\n    <div class=\"left_menu\"><a [routerLink]=\"['Home']\">{{app_name}}</a></div>\n    <div class=\"right_menu\">\n      <span>{{username_label}}<input type=\"text\"></span>\n      <span>{{password_label}}<input type=\"password\"></span>\n      <button class=\"btn\"><a [routerLink]=\"['Profile']\">{{log_in_label}}</a></button>\n    </div>\n   \n    <router-outlet></router-outlet>\n  </div>\n  ",
-                        directives: [router_1.ROUTER_DIRECTIVES],
+                        template: "\n  <div id=\"menu\">\n    <div class=\"left_menu\"><a [routerLink]=\"['Home']\">{{app_name}}</a></div>\n    \n      <div class=\"right_menu\">\n        <form #f=\"ngForm\" (ngSubmit)=\"onSubmit(f.value)\" [hidden] = \"logged_in\">\n          <span>{{username_label}}<input ngControl=\"username\" type=\"text\"></span>\n          <span>{{password_label}}<input ngControl=\"password\" type=\"password\"></span>\n          <button type=\"submit\" class=\"btn\">{{log_in_label}}</button>\n        </form>\n      </div>\n    <pre>sent : {{data}}\n    received : {{resp}} </pre>\n    <router-outlet></router-outlet>\n  </div>\n  ",
+                        directives: [router_1.ROUTER_DIRECTIVES, common_1.FORM_DIRECTIVES],
                     }),
                     router_1.RouteConfig([
                         {
@@ -60,7 +82,7 @@ System.register(['angular2/core', './home.component', './profile.component', './
                             component: register_component_1.RegisterComponent,
                         },
                     ]), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [router_1.Router, http_1.Http])
                 ], AppComponent);
                 return AppComponent;
             }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,23 +2,30 @@ import { Component }       from 'angular2/core';
 import { HomeComponent } from './home.component';
 import { ProfileComponent } from './profile.component';
 import { RegisterComponent } from './register.component';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { Http, Headers, Response} from 'angular2/http'
+import {FORM_DIRECTIVES} from 'angular2/common'
+import { Router, RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'my-app',
   template: `
   <div id="menu">
     <div class="left_menu"><a [routerLink]="['Home']">{{app_name}}</a></div>
-    <div class="right_menu">
-      <span>{{username_label}}<input type="text"></span>
-      <span>{{password_label}}<input type="password"></span>
-      <button class="btn"><a [routerLink]="['Profile']">{{log_in_label}}</a></button>
-    </div>
-   
+    
+      <div class="right_menu">
+        <form #f="ngForm" (ngSubmit)="onSubmit(f.value)" [hidden] = "logged_in">
+          <span>{{username_label}}<input ngControl="username" type="text"></span>
+          <span>{{password_label}}<input ngControl="password" type="password"></span>
+          <button type="submit" class="btn">{{log_in_label}}</button>
+        </form>
+      </div>
+    <pre>sent : {{data}}
+    received : {{resp}} </pre>
     <router-outlet></router-outlet>
   </div>
   `,
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, FORM_DIRECTIVES],
 })
 
 @RouteConfig([
@@ -43,4 +50,33 @@ export class AppComponent {
   username_label = 'Username';
   password_label = "Password";
   log_in_label = "Log In";
+  data: string;
+  resp;
+
+  logged_in = false;
+
+  constructor(
+    private __router : Router,
+    private http: Http
+    ) { }
+
+  onSubmit(data) {
+    var headers = new Headers();
+    headers.append('Contencdt-Type', 'application/json');
+    this.data = JSON.stringify(data);
+    this.http.post(
+      "http://192.168.0.58:8000/users/login/",
+      this.data,
+      headers
+    )
+    .map((res: Response) => JSON.stringify(res.json()))
+    .subscribe(
+      data => this.resp = data,
+      err => this.logError(err)
+     );
+  }
+
+  logError(err) {
+    console.error('There was an error: ' + err);
+  }
 }
